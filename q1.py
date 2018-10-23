42cf24d6a3abd14bbc3895b565a1e07e3e54a97f
Ben Werner <ben.werner01@gmail.com>
2018-10-23 08:45:06 +0100

    small update

--- q1.py
+++ q1.py
@@ -3,7 +3,47 @@
 import numpy as np
 
 # modify this function, and create other functions below as you wish
+
+
 def question01(portfolios):
-  # modify and then return the variable below
-  answer = 0
-  return answer
+	portfolios.sort(reverse=True)
+	print portfolios
+	
+	binaryIndexList = [] 
+
+	x = 0
+	for item in portfolios:
+		binaryNumber = bin(item)[2:]
+		x += 1
+
+		binaryIndexList.append([int(binaryNumber), inverseBinary(binaryNumber)])
+
+
+	if binaryIndexList[0][1] == 0:
+		binaryIndexList.remove(binaryIndexList[0])
+
+#test for perfect matches for longest lengths
+
+	longestLengths = len(str(binaryIndexList[0][0]))
+
+	for binaryNumber in binaryIndexList:
+		if len(str(binaryIndexList))
+
+
+	print binaryIndexList
+	answer = 0
+	return answer
+
+def inverseBinary(num):
+	result = ""
+	for binaryNumber in list(num):
+		if binaryNumber == "0":
+			result += "1"
+		elif binaryNumber == "1":
+			result += "0"
+	return int(result)
+
+
+
+#test
+print question01([15, 8, 6, 7])
